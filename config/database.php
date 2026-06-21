f4424e7c51ffb96b18eb970fd341c2a5448e979b
ErickDalabona <dalabonaerick@gmail.com>
2026-06-21 15:07:41 -0300

    Aula 03: login, sessao e protecao de paginas

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@ $host = "127.0.0.1";
 $db   = "atendelab";
 $user = "root";
 $pass = "";
-$port = "3307";
+$port = "3306";
 try {
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
